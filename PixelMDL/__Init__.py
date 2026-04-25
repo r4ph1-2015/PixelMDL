0cfe67bcf3325303bd96850d5c3414536cbb2955
r4ph1-2015 <142081926+r4ph1-2015@users.noreply.github.com>
2026-04-25 14:19:11 +0200

    Test Commit

---
+++ PixelMDL/__Init__.py
@@ -0,0 +1,29 @@
+import time
+def hello():
+    print("Hello World!")
+
+def sprint():
+    print("What should be printed")
+    text = input()
+
+    print("How many times would you like to repeat")
+    amount = int(input())  
+
+    for i in range(amount):
+        time.sleep(0.05)
+        print(text)
+
+def quiz():
+    answer1 = input("What is your Name?")
+    print("Hello",answer1,",Nice to meet you")
+    answer2 = input("What is the capital of Germany")
+    if answer2 == "Berlin":
+        print("That is correct")
+    else:
+        print("Incorrect")
+    
+    answer3 = input("What coding language do you think this module was built in?")
+    if answer3 == "Python":
+        print("That is correct, Thank you for trying out the Quiz Function")
+    else:
+        print("Actually, It was built in Python. Thank you for trying out the Quiz Function")
---
+++ s.py
@@ -0,0 +1,3 @@
+import pixelmdl as px
+
+px.quiz()
